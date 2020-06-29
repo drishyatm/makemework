b5d6cf5cd7b47bf49fad16ebb578827edff28ef3
Drishya <drishya.tm@qxf2.com>
2020-06-29 20:37:07 +0530

    Temperature was extracted correctly,corrected the filters as sunscreens and moisturizers , Fixed the minimum product

--- page_objects/Main_Page.py
+++ page_objects/Main_Page.py
@@ -4,11 +4,12 @@ URL: /
 """
 from .Base_Page import Base_Page
 from utils.Wrapit import Wrapit
-import conf.locators_conf as locators 
+import conf.locators_conf as locators
+
 
 class Main_Page(Base_Page):
     "Page Object for the main page"
-    #locators
+    # locators
     TEMPERATURE_FIELD = locators.TEMPERATURE_FIELD
     BUY_BUTTON = locators.BUY_BUTTON
     PAGE_HEADING = locators.PAGE_HEADING
@@ -21,46 +22,50 @@ class Main_Page(Base_Page):
     @Wrapit._screenshot
     def get_temperature(self):
         "Return the temperature listed on the landing page"
-        result_flag = False 
+        result_flag = False
         temperature = self.get_text(self.TEMPERATURE_FIELD)
         if temperature is not None:
-            self.write("The temperature parsed is: %s"%temperature,level="debug")
-            #Strip away the degree centigrade
-            temperature = temperature.split()[1] 
-            
+            self.write("The temperature parsed is: %s" %
+                       temperature, level="debug")
+            # Strip away the degree centigrade
+            # changed the selector from 1 to 0
+            temperature = temperature.split()[0]
+
             try:
                 temperature = int(temperature)
             except Exception as e:
-                self.write("Error type casting temperature to int",level="error")
-                self.write("Obtained the temperature %s"%temperature)
+                self.write("Error type casting temperature to int",
+                           level="error")
+                self.write("Obtained the temperature %s" % temperature)
                 self.write("Python says: " + str(e))
             else:
                 result_flag = True
         else:
-            self.write("Unable to read the temperture.",level="")
+            self.write("Unable to read the temperture.", level="")
         self.conditional_write(result_flag,
-        positive="Obtained the temperature: %d"%temperature,
-        negative="Could not obtain the temperature on the landing page.")
-        
+                               positive="Obtained the temperature: %d" % temperature,
+                               negative="Could not obtain the temperature on the landing page.")
+
         return temperature
 
     @Wrapit._exceptionHandler
     @Wrapit._screenshot
-    def click_buy_button(self,product_type):
+    def click_buy_button(self, product_type):
         "Choose to buy moisturizer or sunscreen"
-        result_flag = False 
+        result_flag = False
         product_type = product_type.lower()
-        if product_type in ['sunscreens','moisturizers']:
-            result_flag = self.click_element(self.BUY_BUTTON%product_type)
+        if product_type in ['sunscreens', 'moisturizers']:
+            result_flag = self.click_element(self.BUY_BUTTON % product_type)
             self.conditional_write(result_flag,
-            positive="Clicked the buy button for %s"%product_type,
-            negative="Could not click the buy button for %s"%product_type)
-            result_flag &= self.smart_wait(5,self.PAGE_HEADING%product_type.title())
+                                   positive="Clicked the buy button for %s" % product_type,
+                                   negative="Could not click the buy button for %s" % product_type)
+            result_flag &= self.smart_wait(
+                5, self.PAGE_HEADING % product_type.title())
             if result_flag:
                 self.switch_page(product_type)
 
         self.conditional_write(result_flag,
-        positive="Automation is on the %s page"%product_type.title(),
-        negative="Automation could not navigate to the %s page"%product_type.title())
+                               positive="Automation is on the %s page" % product_type.title(),
+                               negative="Automation could not navigate to the %s page" % product_type.title())
 
         return result_flag
--- page_objects/Product_Object.py
+++ page_objects/Product_Object.py
@@ -6,12 +6,15 @@ from .Base_Page import Base_Page
 import conf.locators_conf as locators
 from utils.Wrapit import Wrapit
 
+
 class Product():
     "A product class"
-    def __init__(self,name,price):
+
+    def __init__(self, name, price):
         "Set up the product with name and price"
-        self.name = name 
-        self.price = price 
+        self.name = name
+        self.price = price
+
 
 class Product_Object():
     "Page Object for the products object"
@@ -23,87 +26,91 @@ class Product_Object():
     CART_QUANTITY = 0
     PRODUCTS_PER_PAGE = 6
 
-    def convert_str_to_int(self,string, default=100000, expect_fail=False):
+    def convert_str_to_int(self, string, default=100000, expect_fail=False):
         "Convert a given string to integer. Return default if you cannot convert"
         try:
             integer = int(string)
         except Exception as e:
             if not expect_fail:
-                self.write("Unable to convert the string %s into a number"%string)
+                self.write(
+                    "Unable to convert the string %s into a number" % string)
             integer = default
 
         return integer
 
     @Wrapit._exceptionHandler
-    def get_product_name(self,product_text):
+    def get_product_name(self, product_text):
         "Parse the product name from the product text"
         name = product_text.split(b"\n")[0].strip()
         name = name.decode('ascii')
 
-        return name 
+        return name
 
     @Wrapit._exceptionHandler
-    def get_product_price(self,product_text):
+    def get_product_price(self, product_text):
         "Parse the product price from the product text"
         price = product_text.split(b"Price: ")[-1]
         price = price.split(b"Rs.")[-1]
         price = price.split(b"\n")[0]
         price = price.decode('ascii')
-        price = self.convert_str_to_int(price,default=100000)
+        price = self.convert_str_to_int(price, default=100000)
 
         return price
 
     @Wrapit._screenshot
     def get_all_products_on_page(self):
         "Get all the products"
-        result_flag = False 
+        result_flag = False
         all_products = []
         product_list = self.get_elements(self.PRODUCTS_LIST)
-        for i,product in enumerate(product_list):
+        for i, product in enumerate(product_list):
             product_text = self.get_dom_text(product)
             name = self.get_product_name(product_text)
             price = self.get_product_price(product_text)
             all_products.append(Product(name, price))
         if self.PRODUCTS_PER_PAGE == len(all_products):
-            result_flag = True 
+            result_flag = True
         self.conditional_write(result_flag,
-        positive="Obtained all %d products from the page"%self.PRODUCTS_PER_PAGE,
-        negative="Could not obtain all products. Automation got %d products while we expected %d products"%(len(all_products),self.PRODUCTS_PER_PAGE))
+                               positive="Obtained all %d products from the page" % self.PRODUCTS_PER_PAGE,
+                               negative="Could not obtain all products. Automation got %d products while we expected %d products" % (len(all_products), self.PRODUCTS_PER_PAGE))
 
         return all_products
-    
+
     def print_all_products(self):
         "Print out all the products nicely"
         all_products = self.get_all_products_on_page()
         self.write("Product list is: ")
         for product in all_products:
-            self.write("%s: %d"%(product.name,product.price))
+            self.write("%s: %d" % (product.name, product.price))
 
-    def get_minimum_priced_product(self,filter_condition):
+    def get_minimum_priced_product(self, filter_condition):
         "Return the least expensive item based on a filter condition"
-        minimum_priced_product = None 
+        minimum_priced_product = None
         min_price = 10000000
         min_name = ''
         all_products = self.get_all_products_on_page()
         for product in all_products:
             if filter_condition.lower() in product.name.lower():
-                if product.price >= min_price:
+                # min price is assigned with max price .so logic should be less than , to work.
+                if product.price <= min_price:
                     minimum_priced_product = product
                     min_price = product.price
-                    min_name = product.name 
-        result_flag = True if minimum_priced_product is not None else False 
+                    min_name = product.name
+        result_flag = True if minimum_priced_product is not None else False
         self.conditional_write(result_flag,
-        positive="Min price for product with '%s' is %s with price %d"%(filter_condition,min_name,min_price),
-        negative="Could not obtain the cheapest product with the filter condition '%s'\nCheck the screenshots to see if there was at least one item that satisfied the filter condition."%filter_condition)
+                               positive="Min price for product with '%s' is %s with price %d" % (
+                                   filter_condition, min_name, min_price),
+                               negative="Could not obtain the cheapest product with the filter condition '%s'\nCheck the screenshots to see if there was at least one item that satisfied the filter condition." % filter_condition)
 
         return minimum_priced_product
-            
-    def click_add_product_button(self,product_name):
+
+    def click_add_product_button(self, product_name):
         "Click on the add button corresponding to the name"
-        result_flag = self.click_element(self.ADD_PRODUCT_BUTTON%product_name)
+        result_flag = self.click_element(
+            self.ADD_PRODUCT_BUTTON % product_name)
         self.conditional_write(result_flag,
-        positive="Clicked on the add button to buy: %s"%product_name,
-        negative="Could not click on the add button to buy: %s"%product_name)
+                               positive="Clicked on the add button to buy: %s" % product_name,
+                               negative="Could not click on the add button to buy: %s" % product_name)
 
         return result_flag
 
@@ -114,18 +121,20 @@ class Product_Object():
         cart_quantity = cart_text.split()[0]
         cart_quantity = cart_quantity.decode('ascii')
         empty_cart_flag = True if self.CART_QUANTITY == 0 else False
-        cart_quantity = self.convert_str_to_int(cart_quantity, default=0, expect_fail = empty_cart_flag)
+        cart_quantity = self.convert_str_to_int(
+            cart_quantity, default=0, expect_fail=empty_cart_flag)
         self.CART_QUANTITY = cart_quantity
         self.conditional_write(True,
-        positive="The cart currently has %d items"%self.CART_QUANTITY,
-        negative="")
+                               positive="The cart currently has %d items" % self.CART_QUANTITY,
+                               negative="")
 
-    def add_product(self,product_name):
+    def add_product(self, product_name):
         "Add the lowest priced product with the filter condition in name"
-        before_cart_quantity = self.get_current_cart_quantity() 
+        before_cart_quantity = self.get_current_cart_quantity()
         result_flag = self.click_add_product_button(product_name)
         after_cart_quantity = self.get_current_cart_quantity()
-        result_flag &= True if after_cart_quantity - before_cart_quantity == 1 else False 
+        result_flag &= True if after_cart_quantity - \
+            before_cart_quantity == 1 else False
 
         return result_flag
 
@@ -134,18 +143,18 @@ class Product_Object():
         "Click the cart button"
         result_flag = self.click_element(self.CART_BUTTON)
         self.conditional_write(result_flag,
-        positive="Clicked on the cart button",
-        negative="Could not click on the cart button")
+                               positive="Clicked on the cart button",
+                               negative="Could not click on the cart button")
 
         return result_flag
 
     @Wrapit._screenshot
     def verify_cart_page(self):
         "Verify automation is on the cart page"
-        result_flag = self.smart_wait(5,self.CART_TITLE)
+        result_flag = self.smart_wait(5, self.CART_TITLE)
         self.conditional_write(result_flag,
-        positive="Automation is on the Cart page",
-        negative="Automation is not able to locate the Cart Title. Maybe it is not even on the cart page?")
+                               positive="Automation is on the Cart page",
+                               negative="Automation is not able to locate the Cart Title. Maybe it is not even on the cart page?")
         if result_flag:
             self.switch_page("main")
 
