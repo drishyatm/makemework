f96873e5bd2839929819fec21f4219915ade8c6c
Drishya <drishya.tm@qxf2.com>
2020-07-07 12:33:48 +0530

    Modified the dictionary to take 2 items to cart

--- conf/e2e_weather_shopper_conf.py
+++ conf/e2e_weather_shopper_conf.py
@@ -2,4 +2,5 @@
 This conf contains the data needed by test_e2e_purchase_product.py
 """
 # edited the arguments with "s" so that it could match the argument passed through the function
-PURCHASE_LOGIC = {"moisturizers": ['aloe'], "sunscreens": ['spf-50']}
+PURCHASE_LOGIC = {"moisturizers": [
+    'aloe', 'almond'], "sunscreens": ['spf-50', 'spf-30']}
